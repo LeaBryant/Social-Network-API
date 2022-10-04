7ec4878091d7ef6729ee1f1f4c9d010b09a63429
LeaBryant <ldbryant3220@gmail.com>
2022-10-03 22:58:32 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Social-Network-API
-build an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list. 
+![license badge](https://img.shields.io/badge/npm-CB3837?style=for-the-badge&logo=npm&logoColor=white)
+
+## Description
+A NoSQL web API to build an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list. 
+
+## Table of Contents
+- [Usage](#usage)
+- [Installation](#Installation)
+- [Deployed](#deployed)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+## Usage
+AS A social media startup
+I WANT an API for my social network that uses a NoSQL database
+SO THAT my website can handle large amounts of unstructured data
+To start the server, in a terminal from the root directory, enter:
+```
+node server.js
+```
+
+Then, test the routes with Insomnia.
+
+## Installation
+Clone the repository and in a terminal from the root directory, enter:
+```
+npm i
+```
+
+```
+Tools Used
+- Express.js
+- MongoDB
+- Mongoose ODM
+- NoSQL
+```
+
+## Deployed
+
+
+## Contributing
+Please reach out if you would like to contribute!😃
+
+## Questions
+Please feel free to explore my other projects on github: [Lea Bryant](https://www.github.com/LeaBryant)
+
+If you have further questions you may send me an email at: ldbryant3220@gmail.com
+
+## Designed by:
+Your's truly Lea Bryant 🦄
+
+
+## Demo
---
+++ models/Thoughts.js
@@ -0,0 +1,77 @@
+const { Schema, model, Types } = require("mongoose");
+//to do: format date
+//const dateFormat = require
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: "Reaction is required",
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: "Username is required",
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        //get: (createdAtVal) => dateFormat(createdAtVal),
+      },
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      //enable getters
+      getters: true,
+    },
+    id: false,
+    _id: false,
+  }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "Thought is required",
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //use getter to format date using the dateFormat() function
+      //get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: "Username is required",
+    },
+
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+////create the model to get the prebuilt methods that Mongoose provides
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
